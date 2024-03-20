8f667957e47593af5085471b0e075dc8e52c4e51
mattwindows <unconfigured@null.spigotmc.org>
2024-03-20 14:22:23 -0400

    a huge amount of placeholder art replaced, things are prettier now

--- Scripts/flicker.gd
+++ Scripts/flicker.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-@onready var light = $PointLight2D
+@onready var light = $"."
 @onready var flickerTimer = $flickerTimer
+@onready var flickerNoise = $lightFlicker
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if randi_range(1, 150) == 50:
 		light.enabled = false
-		flickerTimer.start(randf_range(0.0, 0.03))
+		flickerNoise.pitch_scale = randf_range(0.96, 1.02)
+		flickerNoise.play()
+		flickerTimer.start(randf_range(0.01, 0.03))
 		await flickerTimer.timeout
 		light.enabled = true
